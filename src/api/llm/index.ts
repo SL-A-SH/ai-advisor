11dfa083cb58f7aacc3aaca94df087720b211fec
Aqeeb Rizwan <aqeeb.spider@gmail.com>
2025-09-20 12:18:18 +0530

    Fixed ENV API Path

--- src/api/llm/index.ts
+++ src/api/llm/index.ts
@@ -1,9 +1,9 @@
 export async function queryLLM(prompt: string): Promise<string> {
-  const response = await fetch("", {
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.EXPO_DEV_OPEN_API_KEY}`,
+      Authorization: `Bearer ${process.env.EXPO_PUBLIC_OPEN_API_KEY}`,
     },
     body: JSON.stringify({
       model: "gpt-4o-mini",
@@ -12,7 +12,6 @@ export async function queryLLM(prompt: string): Promise<string> {
   });
 
   if (!response.ok) {
-    console.log(response);
     throw new Error(`LLM API error: ${response.statusText}`);
   }
 
